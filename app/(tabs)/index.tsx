6f24b75e03d9e404f97c81e5f1544441cfebc954
Nathaniel Tampus <nate@mugnavo.com>
2023-11-07 11:20:49 +0800

    search screen with stack navigator

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function index() {
   return (
-    <View style={{ padding: 64, borderWidth: 8, borderColor: "green" }}>
+    <View style={{ padding: 64, borderWidth: 8, borderColor: "green", gap: 16 }}>
       <Text>Index page (or default/home page) for Browse page for now</Text>
+      <Link asChild href="/search">
+        <Button mode="contained">Go to search screen (Stack navigation)</Button>
+      </Link>
     </View>
   );
 }
