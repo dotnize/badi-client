b397806cfb5ca7a627fed6c26cf33d634936a1e0
Nathaniel Tampus <nate@mugnavo.com>
2023-11-15 21:53:53 +0800

    fix: keep current route URL when reloading code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,7 @@
-import { Redirect, Stack } from "expo-router";
+import { Stack } from "expo-router";
 import { MD3LightTheme as DefaultTheme, PaperProvider } from "react-native-paper";
 
 import { COLORS } from "~/lib/theme";
-
-// Temporary ra ang style props diri,
-// mobalhin guro tas StyleSheet kay basin mas limpyo
-// https://reactnative.dev/docs/stylesheet
 
 // Defining routes: <<--- START HERE!
 // https://docs.expo.dev/routing/create-pages/
@@ -16,23 +12,22 @@ import { COLORS } from "~/lib/theme";
 // Layouts:
 // https://docs.expo.dev/routing/layouts/
 
-// ^ All OPTIONAL to read, iexplain ra nako tanan ig meet
-
 const theme = {
   ...DefaultTheme,
   colors: COLORS,
 };
 
-export const unstable_settings = {
-  initialRouteName: "(tabs)",
-};
+// temporarily disabled due to HMR issues
+// export const unstable_settings = {
+//   initialRouteName: "(tabs)",
+// };
 
-const user = undefined; // TEMPORARY
+//const user = undefined; // TEMPORARY
 
 export default function RootLayout() {
   return (
     <PaperProvider theme={theme}>
-      {!user && <Redirect href="/welcome" />}
+      {/* {!user && <Redirect href="/welcome" />} */}
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="welcome" options={{ headerShown: false }} />
