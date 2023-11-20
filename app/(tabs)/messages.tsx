b184737e452065b793f9bb76dc69aaadf38f62b1
leonel christie <leonelchristiebaclayon@gmail.com>
2023-11-20 20:38:46 +0800

    implemented messages screen

--- app/(tabs)/messages.tsx
+++ app/(tabs)/messages.tsx
@@ -1,9 +1,54 @@
+import React, { useState } from "react";
 import { Text, View } from "react-native";
+import { IconButton, List, Searchbar } from "react-native-paper";
 
-export default function messages() {
+interface Message {
+  key: number;
+  avatar: string; // Update the type based on your actual data structure
+  username: string;
+  preview: string;
+}
+
+function MessageListItem({ avatar, username, preview }: Message) {
   return (
-    <View style={{ padding: 64, borderWidth: 8, borderColor: "blue" }}>
-      <Text>Messages page! hi guys</Text>
+    <List.Item
+      title={<Text style={{ fontWeight: "600" }}>{username}</Text>}
+      description={preview}
+      left={() => <List.Icon icon={avatar} />}
+    />
+  );
+}
+
+export default function Messages() {
+  const [searchValue, setSearchValue] = useState("");
+
+  const messages: Message[] = [
+    { key: 1, avatar: "account", username: "Liden U. Hoe", preview: "BAWAWAWAWAW" },
+    { key: 2, avatar: "account", username: "Jameel D. Great", preview: "I love marcy <3." },
+    // Add more messages as needed
+  ];
+
+  return (
+    <View>
+      <View style={{ flexDirection: "row", alignItems: "center", padding: 16 }}>
+        <Searchbar
+          placeholder="Search..."
+          value={searchValue}
+          onChangeText={(query) => setSearchValue(query)}
+          style={{ flex: 1 }}
+        />
+        <IconButton size={20} icon="bell" />
+      </View>
+      <View style={{marginLeft: 18}}>
+        {messages.map((message) => (
+          <MessageListItem
+            key={message.key}
+            avatar={message.avatar}
+            username={message.username}
+            preview={message.preview}
+          />
+        ))}
+      </View>
     </View>
   );
 }
