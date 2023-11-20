608c70ce85e602278363819797de428a622e8f7a
leonel christie <leonelchristiebaclayon@gmail.com>
2023-11-20 13:26:49 +0800

    try ra

--- app/(tabs)/messages.tsx
+++ app/(tabs)/messages.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function messages() {
   return (
     <View style={{ padding: 64, borderWidth: 8, borderColor: "blue" }}>
-      <Text>Messages page!</Text>
+      <Text>Messages page! hi guys</Text>
     </View>
   );
 }
