da44b1dc9f18fbc1c297a717a4d78fc8c1d3debe
Liden <lninyow@gmail.com>
2023-11-21 10:31:18 +0800

    added element dropdown, continued layout, pls freshinstall if mo pull

--- app/(stack)/update.tsx
+++ app/(stack)/update.tsx
@@ -1,36 +1,78 @@
 import { useState } from "react";
 import { Image, View } from "react-native";
-import { List, Text } from "react-native-paper";
-
-const ItemList = () => {
-  const [expanded, setExpanded] = useState(true);
-
-  const handlePress = () => setExpanded(!expanded);
-
-  return (
-    <List.Section>
-      <List.Accordion
-        title="sample title"
-        expanded={expanded}
-        onPress={handlePress}
-        style={{ height: 50 }}
-      >
-        <List.Item title="First item" />
-        <List.Item title="Second item" />
-      </List.Accordion>
-    </List.Section>
-  );
-};
+import { Dropdown } from "react-native-element-dropdown";
+import { Text, TextInput } from "react-native-paper";
 
 export default function Update() {
+  const [details, setDetails] = useState("");
+  const [value, setValue] = useState<string | null>(null);
+  const [isFocus, setIsFocus] = useState(false);
+
+  const items = [
+    { label: "Kanding", value: "1" },
+    { label: "Kabayo", value: "2" },
+    { label: "Itik", value: "3" },
+    { label: "Item 4", value: "4" },
+    { label: "Item 5", value: "5" },
+    { label: "Item 6", value: "6" },
+    { label: "Item 7", value: "7" },
+    { label: "Item 8", value: "8" },
+  ];
+
+  const renderLabel = () => {
+    if (value || isFocus) {
+      return (
+        <Text
+          style={{
+            position: "absolute",
+            backgroundColor: "white",
+            left: 22,
+            top: 8,
+            zIndex: 999,
+            paddingHorizontal: 6,
+            fontSize: 14,
+            color: isFocus ? "lightgreen" : "black",
+          }}
+        >
+          Items
+        </Text>
+      );
+    }
+    return null;
+  };
+
   return (
     <View style={{ flex: 1, alignItems: "center", padding: 8, gap: 8, height: "100%" }}>
       <Text variant="titleMedium">Update Progress</Text>
 
       <View style={{ width: "100%", gap: 8, marginTop: 12 }}>
         <Text>Select item from trade</Text>
-        <View style={{ borderWidth: 1, borderColor: "black", borderRadius: 14 }}>
-          <ItemList />
+        <View style={{ padding: 14 }}>
+          {renderLabel()}
+          <Dropdown
+            style={{
+              height: 50,
+              borderWidth: 0.5,
+              borderRadius: 8,
+              paddingHorizontal: 8,
+              borderColor: isFocus ? "lightgreen" : "gray",
+            }}
+            search
+            maxHeight={250}
+            data={items}
+            value={value}
+            onFocus={() => setIsFocus(true)}
+            onBlur={() => setIsFocus(false)}
+            labelField="label"
+            valueField="value"
+            placeholder={!isFocus ? "Select an item" : "..."}
+            searchPlaceholder="Search..."
+            selectedTextStyle={{ fontSize: 14 }}
+            onChange={(item) => {
+              setValue(item.value);
+              setIsFocus(false);
+            }}
+          />
         </View>
       </View>
 
@@ -43,6 +85,16 @@ export default function Update() {
       </View>
       <View style={{ width: "100%" }}>
         <Text>Details (optional)</Text>
+        <TextInput
+          style={{ flex: 1 }}
+          mode="outlined"
+          label="Details"
+          value={details}
+          onChangeText={(text) => {
+            setDetails(text);
+          }}
+        />
+        <Text>Quantity</Text>
       </View>
     </View>
   );
