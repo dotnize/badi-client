aecc08ab68532e1dec05a684d15542b927a51861
Dead <harenzp14@gmail.com>
2023-11-28 00:54:58 +0800

    Added notifications cards for the notifications screen. Also used the TextInput from react native paper in create new listing screen.

--- app/notifications/index.tsx
+++ app/notifications/index.tsx
@@ -1,12 +1,49 @@
 // URL: /notifications
 
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Card, Paragraph, Title } from "react-native-paper";
+
+const notifications = [
+  { id: 1, title: "Liden", content: "Has sent you an offer!" },
+  { id: 2, title: "Leonel", content: "This is the content of notification 2." },
+  // Add more notifications as needed
+];
+
+const NotificationCard = ({ title, content }) => (
+  <Card style={styles.card}>
+    <Card.Content>
+      <Title>{title}</Title>
+      <Paragraph>{content}</Paragraph>
+    </Card.Content>
+  </Card>
+);
 
 export default function Notifications() {
   return (
-    <View>
-      <Text>Notifications</Text>
+    <View style={styles.container}>
+      {notifications.map((notification) => (
+        <NotificationCard
+          key={notification.id}
+          title={notification.title}
+          content={notification.content}
+        />
+      ))}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  card: {
+    marginVertical: 8,
+  },
+});
