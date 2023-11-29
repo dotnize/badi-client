bd7242bc4a464efb1fe234426384fa4e39ff8eaa
Nathaniel Tampus <nate@mugnavo.com>
2023-11-29 11:16:35 +0800

    update theme & layout for desktop sizes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack, useRouter } from "expo-router";
-import { Appbar, MD3LightTheme as DefaultTheme, PaperProvider } from "react-native-paper";
+import {
+  Appbar,
+  MD3LightTheme,
+  MD3Theme,
+  PaperProvider,
+  adaptNavigationTheme,
+} from "react-native-paper";
 
 import { COLORS } from "~/lib/theme";
 
@@ -12,10 +19,15 @@ import { COLORS } from "~/lib/theme";
 // Layouts:
 // https://docs.expo.dev/routing/layouts/
 
-const theme = {
-  ...DefaultTheme,
+const theme: MD3Theme = {
+  ...MD3LightTheme,
   colors: COLORS,
 };
+
+const { LightTheme } = adaptNavigationTheme({
+  reactNavigationLight: DefaultTheme,
+  materialLight: theme,
+});
 
 // temporarily disabled due to HMR issues
 // export const unstable_settings = {
@@ -30,20 +42,45 @@ export default function RootLayout() {
   return (
     <PaperProvider theme={theme}>
       {/* {!user && <Redirect href="/welcome" />} */}
-      <Stack
-        screenOptions={{
-          header: () => (
-            <Appbar.Header>
-              <Appbar.BackAction
-                onPress={() => (router.canGoBack() ? router.back() : router.replace("/"))}
-              />
-            </Appbar.Header>
-          ),
-        }}
-      >
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="welcome" options={{ headerShown: false }} />
-      </Stack>
+      <ThemeProvider value={LightTheme}>
+        <Stack
+          screenOptions={{
+            contentStyle: {
+              maxWidth: 640,
+              width: "100%",
+              alignSelf: "center",
+            },
+            header: ({ options }) => (
+              // TODO: just use a reusable appbar component to be imported by all stack components
+              <Appbar.Header>
+                <Appbar.BackAction
+                  onPress={() => (router.canGoBack() ? router.back() : router.replace("/"))}
+                />
+                <Appbar.Content title={options.title} />
+              </Appbar.Header>
+            ),
+          }}
+        >
+          <Stack.Screen
+            name="(tabs)"
+            options={{
+              headerShown: false,
+              contentStyle: { maxWidth: "auto" },
+            }}
+          />
+          <Stack.Screen
+            name="welcome"
+            options={{
+              headerShown: false,
+              contentStyle: {
+                maxWidth: 640,
+                width: "100%",
+                alignSelf: "center",
+              },
+            }}
+          />
+        </Stack>
+      </ThemeProvider>
     </PaperProvider>
   );
 }
