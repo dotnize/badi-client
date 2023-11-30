6384c8e44f0bb11f5f13cbc93a312259c79e94a1
Dead <harenzp14@gmail.com>
2023-11-30 19:36:19 +0800

    fixed card content

--- app/match/[id].tsx
+++ app/match/[id].tsx
@@ -15,7 +15,7 @@ export default function MatchFound() {
           <Text style={styles.title}>Suggested Matches</Text>
         </View>
         <Tabs>
-          <TabScreen label="Tab 1">
+          <TabScreen label="To send">
             <ScrollView
               style={{ flex: 1, padding: 8 }}
               showsVerticalScrollIndicator={false}
@@ -61,22 +61,17 @@ export default function MatchFound() {
               {/* Add more CardComponents for suggested matches in Tab 1 */}
             </ScrollView>
           </TabScreen>
-          <TabScreen label="Tab 2">
+          <TabScreen label="To receive">
             <ScrollView
               style={{ flex: 1, padding: 8 }}
               showsVerticalScrollIndicator={false}
               contentContainerStyle={styles.scrollViewContent}
             >
               <CardComponent
-                title="Card 3"
-                username="User 3"
-                content="Content for Card 3"
-                imageSource={require("assets/Kambing.png")} // Replace with actual image source
-              />
-              <CardComponent
-                title="Card 4"
-                username="User 4"
-                content="Content for Card 4"
+                title="Listing #1"
+                username="jameel123"
+                content="Very good kanding, used only once. Shiny and lightweight kanding.
+                        Very good kanding, used only once. Shiny and lightweight kanding."
                 imageSource={require("assets/Kambing.png")} // Replace with actual image source
               />
               {/* Add more CardComponents for suggested matches in Tab 2 */}
@@ -93,10 +88,11 @@ const CardComponent = ({ title, username, content, imageSource }) => (
   <Card style={styles.card}>
     <Card.Cover source={imageSource} style={styles.cardImage} />
     <Card.Content style={styles.cardContent}>
-      <Title>{title}</Title>
-      <Paragraph numberOfLines={1}>{username}</Paragraph>
+      <Title style={styles.cardTitle}>{title}</Title>
+      <Text numberOfLines={1} style={{ color: "grey" }}>
+        {username} &#9733;&#9733;&#9733;&#9733; 4.7 (51)
+      </Text>
       <Paragraph numberOfLines={3}>{content}</Paragraph>
-      {/* Adjust the numberOfLines value based on your design */}
     </Card.Content>
   </Card>
 );
@@ -107,12 +103,18 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     backgroundColor: COLORS.surface,
-    padding: 16,
+    paddingBottom: 16,
+    paddingLeft: 16,
     borderBottomWidth: 1,
     borderBottomColor: "#ccc",
   },
   title: {
-    fontSize: 20,
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: "bold", // Apply bold style
   },
   scrollViewContent: {
     flexDirection: "row",
