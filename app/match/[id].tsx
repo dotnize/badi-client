d8bd3e9050ddac810dd20bd7bdffff8666222a14
Dead <harenzp14@gmail.com>
2023-11-30 17:10:13 +0800

    title and tabscreens

--- app/match/[id].tsx
+++ app/match/[id].tsx
@@ -1,19 +1,45 @@
-// Dynamic route!
-// e.g. /match/1, /match/8, etc.
-
-// Use the useLocalSearchParams hook from expo-router to get the id from the URL.
-
 import { useLocalSearchParams } from "expo-router";
-import { View } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 import { Text } from "react-native-paper";
+import { TabScreen, Tabs, TabsProvider } from "react-native-paper-tabs";
+import { COLORS } from "~/lib/theme";
 
 export default function MatchFound() {
   const { id } = useLocalSearchParams();
-  // If naa natay backend, pwede nato gamiton ang id ig fetch.
 
   return (
-    <View>
-      <Text>MatchFound, id: {id}</Text>
-    </View>
+    <TabsProvider>
+      <View style={styles.container}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Suggested Matches</Text>
+        </View>
+        <Tabs>
+          <TabScreen label="Tab 1">
+            <Text>Content for Tab 1</Text>
+            {/* Add components for suggested matches in Tab 1 */}
+          </TabScreen>
+          <TabScreen label="Tab 2">
+            <Text>Content for Tab 2</Text>
+            {/* Add components for suggested matches in Tab 2 */}
+          </TabScreen>
+        </Tabs>
+      </View>
+    </TabsProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  titleContainer: {
+    backgroundColor: COLORS.surface,
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc", // Add a border at the bottom for separation
+  },
+  title: {
+    fontSize: 20,
+  },
+});
