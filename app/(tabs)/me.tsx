dd4e4af594e0fce55670bbdde1def2b2a5f138af
Nathaniel Tampus <nate@mugnavo.com>
2023-12-02 15:32:27 +0800

    todo: update profile content

--- app/(tabs)/me.tsx
+++ app/(tabs)/me.tsx
@@ -1,40 +1,8 @@
-import { useEffect, useState } from "react";
 import ProfileContent from "~/components/profile-content";
-import { API_URL } from "~/lib/config";
+import { useSession } from "~/hooks/useSession";
 
 export default function MyProfile() {
-  // a call to get the data of user:
-  // values needed: name, ratings, pfppic, coverpic, inventoryList, wishesList, historyList
-  const user = "sample"; // a call to db that returns a user type values needed
-  const isLoggeduser = true; // logic  to check if user is the loggedinUser
+  const { user } = useSession();
 
-  const [wishes, setWishes] = useState<null | object>();
-  const [inventory, setInventory] = useState<null | object>();
-  const [history, setHistory] = useState<null | object>();
-
-  async function fetchUserList(
-    url: string,
-    setData: React.Dispatch<React.SetStateAction<object | null | undefined>>
-  ) {
-    const res = await fetch(`${API_URL}/${url}`);
-    if (!res.ok) return setWishes(null);
-
-    const data = await res.json();
-
-    if (!data) {
-      console.log("sadge");
-      setData(null);
-    } else {
-      console.log(typeof data);
-      setData(data);
-    }
-  }
-
-  useEffect(() => {
-    fetchUserList("wish", setWishes);
-    // fetchUserList("inventory", setInventory);
-    // fetchUserList("history", setHistory);
-  }, []);
-
-  return <ProfileContent user={user} isLoggedUser={isLoggeduser} wishes={wishes} />;
+  return <ProfileContent userId={user?.id} />;
 }
