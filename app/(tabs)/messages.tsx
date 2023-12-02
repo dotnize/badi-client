b3b861f7b4d5733dd5680bd0ba45d89a85991591
leonel christie <leonelchristiebaclayon@gmail.com>
2023-12-02 13:45:53 +0800

    minor fix

--- app/(tabs)/messages.tsx
+++ app/(tabs)/messages.tsx
@@ -47,7 +47,7 @@ export default function Messages() {
 
   useEffect(() => {
     fetchConversations();
-  }, []);
+  }, [user]);
 
   return (
     <View>
--- app/user/[id]/index.tsx
+++ app/user/[id]/index.tsx
@@ -1,10 +1,16 @@
+import { useLocalSearchParams } from "expo-router";
 import ProfileContent from "~/components/profile-content";
 
-export default function MyProfile() {
+export default function OtherUserProfile() {
   // a call to get the data of user:
   // values needed: name, ratings, pfppic, coverpic, inventoryList, wishesList, historyList
-  const user = "sample"; // a call to db that returns a user type values needed
-  const isLoggeduser = false; // a call to check if user is the loggedinUser
+  //const user = "sample"; // a call to db that returns a user type values needed
+  //const isLoggeduser = false; // a call to check if user is the loggedinUser
+  const { id } = useLocalSearchParams();
 
-  return <ProfileContent user={user} isLoggedUser={isLoggeduser} />;
+  // eslint-disable-next-line radix
+  const idFromUrl = parseInt(id as string);
+
+  return <ProfileContent userId={idFromUrl} />;
+  //return <ProfileContent user={user} isLoggedUser={isLoggeduser} />;
 }
