6ea8707026d627d87c7eab019a81d7ead7343398
Nathaniel Tampus <nate@mugnavo.com>
2023-12-02 10:50:19 +0800

    add more logs when initializing session context

--- context/ContextProvider.tsx
+++ context/ContextProvider.tsx
@@ -8,11 +8,14 @@ export default function ContextProvider({ children }: { children: React.ReactNod
   const [user, setUser] = useState<User | null | "loading">("loading");
 
   async function fetchUser() {
+    console.log("Checking currently logged in user...");
     const res = await apiFetch<User>(`/auth`);
 
-    if (!res.data?.id || res.error) {
+    if (!res.data || !res.data.id || res.error) {
+      console.log("No logged-in user, session user is null!");
       setUser(null);
     } else {
+      console.log(`Session user is valid - email: ${res.data.email}, id: ${res.data.id}`);
       setUser(res.data);
     }
   }
