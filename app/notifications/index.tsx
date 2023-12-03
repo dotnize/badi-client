906fc674149c38e80c170713aa49648865d64c14
Dead <harenzp14@gmail.com>
2023-12-03 22:11:40 +0800

    updated the card props and interface with appropriate properties

--- app/notifications/index.tsx
+++ app/notifications/index.tsx
@@ -1,63 +1,70 @@
 // URL: /notifications
 
-import React, { useState } from "react";
+import React from "react";
 import { StyleSheet, View } from "react-native";
-import { Avatar, Button, Card, IconButton, Paragraph, Title } from "react-native-paper";
-
-const initialNotifications = [
-  { id: 1, title: "Liden", content: "has sent you an offer!" },
-  { id: 2, title: "Leonel", content: "Looking for a secondhand airplane." },
-];
+import { Avatar, Card, IconButton } from "react-native-paper";
 
 interface NotificationCardProps {
   id: number;
-  title: string;
-  content: string;
+  type: string;
+  content: {
+    // Adjust the structure based on your JSON structure for content
+    // Example: Adjust this based on your JSON structure: title: string; message: string;
+  };
+  timestamp: string; // Assuming timestamp is a string, adjust accordingly
+  is_read: boolean;
+  is_deleted: boolean;
+  user_id: number;
+  userName: string; // Adding userName to represent the user's name
   onRemove: (id: number) => void;
 }
 
-const NotificationCard: React.FC<NotificationCardProps> = ({ id, title, content, onRemove }) => (
+const NotificationCard: React.FC<NotificationCardProps> = ({
+  id,
+  type,
+  content,
+  timestamp,
+  is_read,
+  is_deleted,
+  user_id,
+  userName,
+  onRemove,
+}) => (
   <Card style={styles.card}>
     <Card.Content style={styles.cardContent}>
       <Avatar.Icon size={48} icon="account-circle" />
-      <View style={styles.textContainer}>
-        <Title>{title}</Title>
-        <Paragraph>{content}</Paragraph>
-      </View>
+      <View style={styles.textContainer}></View>
       <IconButton icon="close" onPress={() => onRemove(id)} />
     </Card.Content>
   </Card>
 );
 
 export default function Notifications() {
-  const [notifications, setNotifications] = useState(initialNotifications);
-
-  const removeNotification = (id: number) => {
-    const updatedNotifications = notifications.filter((notification) => notification.id !== id);
-    setNotifications(updatedNotifications);
-  };
-
-  const clearAllNotifications = () => {
-    setNotifications([]);
-  };
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Title style={styles.header}>Notifications</Title>
-        <Button onPress={clearAllNotifications}>Clear All</Button>
-      </View>
-      {notifications.map((notification) => (
-        <NotificationCard
-          key={notification.id}
-          id={notification.id}
-          title={notification.title}
-          content={notification.content}
-          onRemove={removeNotification}
-        />
-      ))}
-    </View>
-  );
+  // const [notifications, setNotifications] = useState();
+  // const removeNotification = (id: number) => {
+  //   const updatedNotifications = notifications.filter((notification) => notification.id !== id);
+  //   setNotifications(updatedNotifications);
+  // };
+  // const clearAllNotifications = () => {
+  //   setNotifications([]);
+  // };
+  // return (
+  //   <View style={styles.container}>
+  //     <View style={styles.headerContainer}>
+  //       <Title style={styles.header}>Notifications</Title>
+  //       <Button onPress={clearAllNotifications}>Clear All</Button>
+  //     </View>
+  //     {notifications.map((notification) => (
+  //       <NotificationCard
+  //         key={notification.id}
+  //         id={notification.id}
+  //         title={notification.title}
+  //         content={notification.content}
+  //         onRemove={removeNotification}
+  //       />
+  //     ))}
+  //   </View>
+  // );
 }
 
 const styles = StyleSheet.create({
